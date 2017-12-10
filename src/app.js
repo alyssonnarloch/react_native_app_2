1eb2b0059f8f8044027c7ae6e14a5966ea26b5db
Alysson Narloch <alysson@localhost.localdomain>
2017-12-10 15:57:10 -0200

    Criação do formulário com email e carregamento do firebase.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDRwcymwjxSdfad-85E1uj6-LKh-Zv0hLU',
+            authDomain: 'react-native-auth-ce191.firebaseapp.com',
+            databaseURL: 'https://react-native-auth-ce191.firebaseio.com',
+            projectId: 'react-native-auth-ce191',
+            storageBucket: 'react-native-auth-ce191.appspot.com',
+            messagingSenderId: '440569175413'
+        });   
+    }
+
     render() {
         return ( 
             <View>
                 <Header headerText="Authentication" />
-                <Text>An app</Text>
+                <LoginForm />
             </View>
         );
     }
