99a61b0c71b8cb80da2ac58310226697f8c844f6
Alysson Narloch <alysson@localhost.localdomain>
2017-12-10 17:39:01 -0200

    Cria usuário e valida email e senha.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { text: '' };
+    state = { email: '', password: '', error: '' };
+    
+    onButtonPress() {
+        const { email, password } = this.state;
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication Failed' });
+                    });
+            });
+    }
 
     render() {
         return (
@@ -11,15 +24,28 @@ class LoginForm extends Component {
                 <CardSection>
                     <Input 
                         label="Email"
-                        value={this.state.text}
-                        onChangeText={text => this.setState({ text })}
+                        placeholder="user@gmail.com"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
                     />
                 </CardSection>
 
-                <CardSection />
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        label="Password"
+                        placeholder="password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
+
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
 
                 <CardSection>
-                    <Button>
+                    <Button propOnPress={this.onButtonPress.bind(this)}>
                         Log in
                     </Button>
                 </CardSection>
@@ -27,5 +53,13 @@ class LoginForm extends Component {
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
 
 export default LoginForm;
