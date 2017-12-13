3b2b2d34556f64b4ffe9d36b56af8ce0206bd86f
Alysson Narloch <alysson@localhost.localdomain>
2017-12-13 20:35:44 -0200

    Logout e spinners para mostrar ao usuário que algo está acontecendo.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Card, CardSection, Button, Header, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyDRwcymwjxSdfad-85E1uj6-LKh-Zv0hLU',
@@ -13,14 +15,45 @@ class App extends Component {
             projectId: 'react-native-auth-ce191',
             storageBucket: 'react-native-auth-ce191.appspot.com',
             messagingSenderId: '440569175413'
-        });   
+        }); 
+        
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Button propOnPress={() => firebase.auth().signOut()}>Log Out</Button>
+                        </CardSection>
+                    </Card>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return (
+                    <Card>
+                        <CardSection>
+                            <Spinner size="large" />
+                        </CardSection>
+                    </Card>
+            );
+        }
     }
 
     render() {
         return ( 
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
